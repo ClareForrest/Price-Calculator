d60df1ee63769274c75c3dfa8c7739be4ac0faa4
Redbubble Applicant <me@example.com>
2021-09-23 20:22:13 +1000

    update error handling

--- index.js
+++ index.js
@@ -8,19 +8,20 @@ let database
 processArgv()
 readingFiles(cartFile)
 
+// TO DO: modify function for windowsOS and sanitise file so '\' isn't required
 // Process the arguments passed to CLI and get filepath for the passed in file
 function processArgv(){
   if(process.argv[2]){
     dbFileArg = process.argv[2]
     database = path.join(__dirname, dbFileArg)
   } else {
-    throw Error("Missing/invalid database file, please ensure file is provided in '/file-name.json' format")
+    throw Error("Missing/invalid database file, please ensure file is provided in 'file-name.json' format")
   }
   if(process.argv[3]){
     cartFileArg = process.argv[3]
     cartFile = path.join(__dirname, cartFileArg)
   } else {
-    throw Error("Missing/invalid cart file, please ensure file is in '/file-name.json' format")
+    throw Error("Missing/invalid cart file, please ensure file is in 'file-name.json' format")
   }
 }
 // Read the cart file
@@ -28,7 +29,7 @@ function processArgv(){
 function readingFiles (cartFile){
   readFile(cartFile, 'utf-8', function(err, jsonString) {
     if(err){ 
-      console.log('File read failed:', err)
+      console.log('File read failed:', err, "Likely invalid cart file, please ensure file is in the 'file-name.json' format")
     } else {
       let cart = JSON.parse(jsonString)
 
